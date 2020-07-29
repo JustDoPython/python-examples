aa54ff577fab802ae5ec2b8f0fb6ae5f558dd492
rory0807 <2230363024@qq.com>
2020-07-29 17:13:03 +0800

    no message

--- moumoubaimifan/kouhong/kouhong.py
+++ moumoubaimifan/kouhong/kouhong.py
@@ -4,9 +4,9 @@ import requests
 from PIL import Image, ImageDraw
 
 
-ak = 'MbDXGOrgXHqsgHKlAZLv6K93'
+ak = 'xx'
 
-sk = 'fzIOiK2aEAKntAY7cOEHkUCoZOawe0wR'
+sk = 'xx'
 
 # client_id 为官网获取的AK， client_secret 为官网获取的SK
 host = 'https://aip.baidubce.com/oauth/2.0/token?grant_type=client_credentials&client_id='+ak+'&client_secret=' + sk
