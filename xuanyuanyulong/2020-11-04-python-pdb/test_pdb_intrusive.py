a7a1b55fc74d9e95c5f1044fd1a942e9416ac0a7
lierchao1997 <lierchao1997@gmail.com>
2020-11-05 04:23:07 +0800

    提交示例代码

---
+++ xuanyuanyulong/2020-11-04-python-pdb/test_pdb_intrusive.py
@@ -0,0 +1,23 @@
+import pdb; # pdb.set_trace()
+
+
+def addStr(a, b):
+    return a + b
+
+def mergeChar(l: list):
+    return ''.join(l)
+
+def getSlogan(a, b, c):
+    result = addStr(a, b) + mergeChar(c)
+    return result
+
+
+a = "just"
+
+b = "do"
+
+pdb.set_trace()
+
+c = ['p', 'y', 't', 'h', 'o', 'n']
+
+print(a)
